24fe5cac44d28e721f9c40d8c4bf23ab24ae5403
Elliotzx_ <102184939+Elliot-developer@users.noreply.github.com>
2024-10-11 12:01:07 -0300

    adasd

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
-    cartao.classname = 'cartao';
-    cartao.innerHTML = '
+    cartao.classname = 'cartao'
+    cartao.innerHTML = `
     <div class="conteudo-cartao">
     <h3>categoria</h3>
     <div class="pergunta-cartao">
@@ -12,6 +12,6 @@ function criaCartao(categoria, pergunta, resposta) {
         <p>resposta</p>
     </div>
     </div>
-    '
+    `
     
 }
